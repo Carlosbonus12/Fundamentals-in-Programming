783349c7dae8d4d713e49de8edf77d5d267e8a47
Carlosbonus12 <2025-1-01460@lpu.edu.ph>
2025-09-30 09:11:00 +0800

    Add files via upload

---
+++ 100 names.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int name = 0;
+    
+    do {
+        printf("Carlos G. Bonus\n");
+        name++;
+    } while (name < 100);
+    
+    return 0;
+}
---
+++ Countdown.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int countdown = 150;
+    
+    while (countdown >= 0) {
+        printf("%d\n", countdown);
+        countdown--;
+        
+    }
+    return 0;
+    
+}
