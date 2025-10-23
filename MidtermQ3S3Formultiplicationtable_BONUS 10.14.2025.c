29de736c4f91120f5c8eda1de29ae04b48ba0ffd
Carlosbonus12 <2025-1-01460@lpu.edu.ph>
2025-10-24 07:35:43 +0800

    Expand multiplication table to 1-1000 for 9

--- MidtermQ3S3Formultiplicationtable_BONUS 10.14.2025.c
+++ MidtermQ3S3Formultiplicationtable_BONUS 10.14.2025.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int i;
+    int ck;
 
-    for (i = 1; i <= 10; i++) {
-        printf("9 x %d = %d\n", i, 9 * i);
+    for (ck = 1; ck <= 1000; ck++) {
+        printf("9 x %d = %d\n", ck, 9 * ck);
     }
 
     return 0;
