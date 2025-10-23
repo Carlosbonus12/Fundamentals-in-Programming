21143455b99b40e082a64190d94f7764d5c29c10
Carlosbonus12 <2025-1-01460@lpu.edu.ph>
2025-10-24 07:35:57 +0800

    Change loop to print odd numbers up to 9999999
    Modified loop to print odd numbers up to 9999999 instead of 10.

--- MidtermQ3S3Foroddnumbers_BONUS 10.14.2025.c
+++ MidtermQ3S3Foroddnumbers_BONUS 10.14.2025.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int i;
+    int ck;
     
-    for (i = 1; i <= 10; i = i + 2) {
-        printf("%d\n", i);
+    for (ck = 1; ck <= 9999999; ck = ck + 2) {
+        printf("%d\n", ck);
     }
     
     return 0;
